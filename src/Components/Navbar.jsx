5b0626378f66fe0eff23086e2b4ac003af7f82bb
Tamani Phiri <tamanigabriel0@gmail.com>
2023-05-08 14:38:13 +0200

    nav menu under way

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,23 +7,33 @@ const Navbar = () => {
     <div>
         <nav className="fixed top-0 left-0 w-full bg-orange-400 shadow">
             <div className="container px-4 md:px-12 py-4 m-auto flex items-center justify-between">
+
+                {/*LOGO*/}
                 <h1 className='font-extrabold font text-orange-900 cursor-pointer hover:scale-105'>
                     Ula<span className=' italic text-white'>i</span>
                 </h1>
+
+                {/*Menu List*/}
                 <ul className="hidden md:flex items-center text-white">
                     <li className="hover:text-orange-900 cursor-pointer">Home</li>
                     <li className="pl-6 hover:text-orange-900 cursor-pointer">Services</li>
                     <li className="pl-6 hover:text-orange-900 cursor-pointer">Pricing</li>
                 </ul>
+
+                {/*Menu Button*/}
                 <button className='block md:hidden'>
+
+                    {/*Button*/}
                     <div className="w-5 h-[2px] bg-orange-900 mb-1"></div>
                     <div className="w-5 h-[2px] bg-orange-900 mb-1"></div>
                     <div className="w-5 h-[2px] bg-orange-900 mb-1"></div>
+
+                    {/*List*/}
                     <div className=" w-3/4 absolute top-0 right-0 h-screen bg-white border">
                         <ul>
-                            <li className="px-6 py-4 border-b-[1px] hover:text-orange-900 cursor-pointer">Home</li>
-                            <li className="px-6 py-4 border-b-[1px] hover:text-orange-900 cursor-pointer">Services</li>
-                            <li className="px-6 py-4 border-b-[1px] hover:text-orange-900 cursor-pointer">Pricing</li>
+                            <li className="px-6 py-4 border-b-[1px] hover:bg-orange-300 cursor-pointer">Home</li>
+                            <li className="px-6 py-4 border-b-[1px] hover:bg-orange-300 cursor-pointer">Services</li>
+                            <li className="px-6 py-4 border-b-[1px] hover:bg-orange-300 cursor-pointer">Pricing</li>
                         </ul>
                     </div>
                 </button>
