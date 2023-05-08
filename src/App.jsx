aa0bc9e7de91441481c8b28d2f56b18ba7a0f081
Tamani Phiri <tamanigabriel0@gmail.com>
2023-05-08 15:56:30 +0200

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
+import Hero from './Components/Hero'
 import Navbar from './Components/Navbar'
+import Price from './Components/Price'
 
 function App() {
 
   return (
-    <>
+    <div className=" items-center justify-center">
       <Navbar/>
-    </>
+      <Hero/>
+      <Price/>
+    </div>
   )
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-import Hero from './Hero';
 
 const Navbar = () => {
 
-    const handleClick = () => {
-        const ref = document.getElementById('hero');
+    const services = () => {
+        const ref = document.getElementById('services');
         ref?.scrollIntoView({ behavior: 'smooth' });
       };
 
   return (
-    <div>
+    <section className=' pb-8'>
         <nav className="fixed top-0 left-0 w-full bg-orange-400 shadow">
             <div className="container px-4 md:px-12 py-4 m-auto flex items-center justify-between">
 
@@ -22,7 +21,7 @@ const Navbar = () => {
                 {/*Menu List*/}
                 <ul className="hidden md:flex items-center text-white">
                     <li className="hover:text-orange-900 cursor-pointer">Home</li>
-                    <li className="pl-6 hover:text-orange-900 cursor-pointer">Services</li>
+                    <li className="pl-6 hover:text-orange-900 cursor-pointer" onClick={services}>Services</li>
                     <li className="pl-6 hover:text-orange-900 cursor-pointer">Pricing</li>
                 </ul>
 
@@ -38,15 +37,14 @@ const Navbar = () => {
                     <div className=" w-3/4 absolute top-0 -right-full h-[600px] bg-white border opacity-0 group-focus:right-0 group-focus:opacity-100 transition-all duration-300">
                         <ul>
                             <li className="px-6 py-4 border-b-[1px] hover:bg-orange-300 cursor-pointer">Home</li>
-                            <li className="px-6 py-4 border-b-[1px] hover:bg-orange-300 cursor-pointer" onClick={handleClick}>Services</li>
+                            <li className="px-6 py-4 border-b-[1px] hover:bg-orange-300 cursor-pointer" onClick={services}>Services</li>
                             <li className="px-6 py-4 border-b-[1px] hover:bg-orange-300 cursor-pointer">Pricing</li>
                         </ul>
                     </div>
                 </button>
             </div>
         </nav>
-        <Hero/>
-    </div>
+    </section>
   )
 }
 
