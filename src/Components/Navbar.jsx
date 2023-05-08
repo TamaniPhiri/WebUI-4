ea38cbebfbb99a19698cf9bc960a42d7b35ff76d
Tamani Phiri <tamanigabriel0@gmail.com>
2023-05-08 15:02:46 +0200

    hero section

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
                     <div className="w-5 h-[2px] bg-orange-900 mb-1"></div>
 
                     {/*List*/}
-                    <div className=" w-3/4 absolute top-0 -right-full h-screen bg-white border opacity-0 group-focus:right-0 group-focus:opacity-100 transition-all duration-300">
+                    <div className=" w-3/4 absolute top-0 -right-full h-[600px] bg-white border opacity-0 group-focus:right-0 group-focus:opacity-100 transition-all duration-300">
                         <ul>
                             <li className="px-6 py-4 border-b-[1px] hover:bg-orange-300 cursor-pointer">Home</li>
                             <li className="px-6 py-4 border-b-[1px] hover:bg-orange-300 cursor-pointer">Services</li>
