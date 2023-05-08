ab07ad984a039b8c46c1e67110b54fbe648beb20
Tamani Phiri <tamanigabriel0@gmail.com>
2023-05-08 18:18:24 +0200

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from './Components/Navbar'
 function App() {
 
   return (
-    <div className=" px-4 md:px-12">
+    <div className=" px-4 md:px-12 container">
       <Navbar/>
       <Hero/>
     </div>
