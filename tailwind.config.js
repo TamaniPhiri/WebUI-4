bfc8c52fc17146f88e56a821afc69b957d4ca481
Tamani Phiri <tamanigabriel0@gmail.com>
2023-05-08 14:45:41 +0200

    nav menu under way

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ export default {
   theme: {
     extend: {},
   },
+  variants:{
+    extend:{
+      display:['group-focus'],
+      opacity:['group-focus'],
+      inset:['group-focus']
+    }
+  },
   plugins: [],
 }
 
