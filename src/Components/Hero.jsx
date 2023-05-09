53ece59f8d1eda94d2c47e5490c3f0bd95c4f6f0
Tamani Phiri <tamanigabriel0@gmail.com>
2023-05-09 12:54:20 +0200

    hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -8,12 +8,12 @@ const Hero = () => {
   return (
     <section className="flex w-full h-screen md:h-fit mt-20 md:mt-20 flex-col-reverse md:flex-row items-center justify-center px-4 md:px-14 container">
         <div className="w-full md:w-1/2">
-            <h1 className=' text-[#3c4c4c] font-bold text-3xl md:text-5xl'>Lorem Ipsum Dolor</h1>
+            <h1 className=' text-[#3c4c4c] font-bold text-3xl md:text-5xl text-center md:text-left'>Lorem Ipsum Dolor</h1>
             <p className='text-[#939c9c] mt-4 text-sm md:text-lg text-center md:text-left'>
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non excepturi dolores corrupti adipisci, ab neque natus hic explicabo commodi!
             </p>
             <div className="items-center flex-row w-full flex mx-auto py-7 space-x-4">
-                <div className="bg-[#085e57] items-center justify-center w-60 rounded-md py-2 text-center">
+                <div className="bg-[#085e57] items-center justify-center w-full md:w-60 rounded-md py-2 text-center">
                     <a href="#" className=' px-4 text-white'>Get Started</a>
                 </div>
                 <div className="hidden md:block shadow-md items-center justify-center w-60 rounded-md py-2 mx-0 md:mx-4 text-center">
